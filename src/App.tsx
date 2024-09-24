3d0468b811bab5dd09a4ce40c2d843bd5dc452e9
Ignacio M Arteaga P <ignacio.moises.arteaga.pelcast@gmail.com>
2024-09-24 11:02:10 -0600

    feat(styles): Firsts configurations and styles were added

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+function App() {
+    return (
+        <>
+            <div className="container">
+                <h1 className="app-title">
+                    <span>Cryptocurrency</span> quoter
+                </h1>
+                <div className="content"></div>
+            </div>
+        </>
+    );
+}
+
+export default App;
