81e77bb29b020d2bafc787dfc21881ea10989364
Ignacio M Arteaga P <ignacio.moises.arteaga.pelcast@gmail.com>
2024-09-24 15:50:45 -0600

    feat(components,data,schema,types):The CryptoSearch component, index data, zod crypto schema, global types and the zustand store were added

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,23 @@
+import { useEffect } from "react";
+import { CryptoSearchFrom } from "./components/CryptoSearchFrom";
+import { useCryptoStore } from "./store";
+
 function App() {
+    const fetchCryptos = useCryptoStore((state) => state.fetchCryptos);
+
+    useEffect(() => {
+        fetchCryptos();
+    }, []);
+
     return (
         <>
             <div className="container">
                 <h1 className="app-title">
                     <span>Cryptocurrency</span> quoter
                 </h1>
-                <div className="content"></div>
+                <div className="content">
+                    <CryptoSearchFrom />
+                </div>
             </div>
         </>
     );
---
+++ src/data/index.ts
@@ -0,0 +1,7 @@
+import { Currency } from "../types";
+
+export const currencies : Currency[] = [
+  { code: 'USD', name: 'US Dollar'},
+  { code: 'MXN', name: 'Mexican Peso'},
+  { code: 'EUR', name: 'Euro'},
+]
